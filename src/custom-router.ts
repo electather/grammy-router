6bb921004e2bf917702f168e6accb6cb4b4539f1
Omid Astaraki <omidocean@gmail.com>
2023-08-24 15:36:32 +0100

    reduce generics

--- src/custom-router.ts
+++ src/custom-router.ts
@@ -6,20 +6,21 @@ import {
     MiddlewareObj,
 } from './deps.deno.ts'
 
-type RouteMatcher<T extends Context> = (ctx: T) => PropertyKey
+type RouteMatcher<T> = (ctx: T) => PropertyKey
 
-export class CustomRouter<C extends Context, RM extends RouteMatcher<C>>
-    implements MiddlewareObj<C>
-{
+export class CustomRouter<C extends Context> implements MiddlewareObj<C> {
     private readonly routeHandlers: Partial<
-        Record<ReturnType<RM>, Middleware<C>>
+        Record<ReturnType<typeof this.routeMatcher>, Middleware<C>>
     > = {}
 
-    constructor(private readonly routeMatcher: RM) {
+    constructor(private readonly routeMatcher: RouteMatcher<C>) {
         this.routeMatcher = routeMatcher
     }
 
-    route(route: ReturnType<RM>, ...middleware: Array<Middleware<C>>) {
+    route(
+        route: ReturnType<typeof this.routeMatcher>,
+        ...middleware: Array<Middleware<C>>
+    ) {
         const composer = new Composer(...middleware)
         this.routeHandlers[route] = composer
         return composer
